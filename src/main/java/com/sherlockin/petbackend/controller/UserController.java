5c0dac02b4835ecf43d5bfcb73862e11f97d8aa9
Kalleo Carrilho <kalleocarrilho@gmail.com>
2026-05-11 20:33:02 -0300

    feat: implement JWT authentication and user registration functionality

---
+++ src/main/java/com/sherlockin/petbackend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.sherlockin.petbackend.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sherlockin.petbackend.model.User;
+import com.sherlockin.petbackend.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        return ResponseEntity.ok(userService.listUsers());
+    }
+
+}
